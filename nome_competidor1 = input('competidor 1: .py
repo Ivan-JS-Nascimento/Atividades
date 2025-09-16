962aee99150694ee8cf6e2e92d639fca8b46ea2d
ijsn <ijsn@g3c19.cin.ufpe.br>
2025-09-16 07:29:27 -0300

    at 01

---
+++ nome_competidor1 = input('competidor 1: .py
@@ -0,0 +1,16 @@
+nome_competidor1 = input('competidor 1: ')
+
+pasteis_competidor1 = int(input('Numero pasteis 1: '))
+
+nome_competidor2 = input('competidor 2: ')
+
+pasteis_competidor2 = int(input('Numero pasteis 2: '))
+
+nome_competidor3 = input('competidor 3: ')
+
+pasteis_competidor3 = int(input('Numero pasteis 3: '))
+
+if nome_competidor1.lower() or nome_competidor2.lower() or nome_competidor3.lower() == 'lineu':
+    print('Lineu comeu um pastel com gosto estranho e usou sua autoridade na vigilancia sanitaria para acabar com a competição, Beiçola tá desolado!')
+else:
+    print('teste')
