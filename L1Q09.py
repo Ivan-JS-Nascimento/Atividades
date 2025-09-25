2c39a16ac522501470dee96b0db0b29c657f76f4
Ivan-JS-Nascimento <ijsn@cin.ufpe.br>
2025-09-25 19:38:38 -0300

    Atualizar o L1Q09.py

--- L1Q09.py
+++ L1Q09.py
@@ -3,6 +3,8 @@ p1 = input()
 p2 = input()
 p3 = input()
 p4 = input()
+per_b = ''
+per_c = ''
 if 'Terry' in [p1,p2,p3,p4] or 'Holt' in [p1,p2,p3,p4]:
     print('Jimmy Jab CANCELADO!')
 else:
@@ -11,27 +13,11 @@ else:
 
     if 'Scully' in [p1,p2,p3,p4]:
         per_b=input()
-        if p1 == per_b:
-            p1=''
-        elif p2 == per_b:
-            p2=''
-        elif p3 == per_b:
-            p3=''
-        elif p4 == per_b:
-            p4=''
         print('Scully leva a melhor, não tem como competir com ele.')
         print(f'{per_b} não avançou para a próxima fase!')
     else:
         ven_b=input()
         per_b=input()
-        if p1 == per_b:
-            p1=''
-        elif p2 == per_b:
-            p2=''
-        elif p3 == per_b:
-            p3=''
-        elif p4 == per_b:
-            p4=''
         print(f'{ven_b} levou a melhor na Bocatona!')
         print(f'{per_b} não avançou para a próxima fase!')
 
@@ -41,5 +27,18 @@ else:
     t3=int(input())
     maior = t1 if (t1 > t2) and (t1 > t3) else t2 if (t2 > t3) else t3
     menor = t1 if (t1 < t2) and (t1 < t3) else t2 if (t2 < t3) else t3
-    ven_b 
-    per_b=input()
+    
+    if p1==per_b:
+    	ven_c = p2 if (t1==menor) else p3 if (t2==menor) else p4 
+    	per_c = p2 if (t1==maior) else p3 if (t2==maior) else p4
+    elif p2==per_b:
+    	ven_c = p1 if (t1==menor) else p3 if (t2==menor) else p4
+    	per_c = p1 if (t1==maior) else p3 if (t2==maior) else p4
+    elif p3==per_b:
+    	ven_c = p1 if (t1==menor) else p2 if (t2==menor) else p4
+    	per_c = p1 if (t1==maior) else p2 if (t2==maior) else p4
+    else:
+    	ven_c = p1 if (t1==menor) else p2 if (t2==menor) else p3
+    	per_c = p1 if (t1==maior) else p2 if (t2==maior) else p3
+	print(f'{ven_c} levou a melhor na Corrida Volumosa!')
+	print(f'')
