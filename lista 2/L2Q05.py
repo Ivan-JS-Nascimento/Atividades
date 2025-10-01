faeff55c57555f2342423d495ed960087b1058de
Ivan-JS-Nascimento <ijsn@cin.ufpe.br>
2025-10-01 17:48:16 -0300

    Atualizar o L2Q05.py

--- lista 2/L2Q05.py
+++ lista 2/L2Q05.py
@@ -3,11 +3,10 @@ b_bruno=int(input())
 b_clara=int(input())
 op=True
 while(op):
-    l = 3 if(b_andre!=0 and b_bruno!=0 and b_clara!=0) else 2
-    for i in range(l):
-        a=True if(b_andre!=0) else False
-        b=True if(b_bruno!=0) else False
-        c=True if(b_clara!=0) else False
+    a=True if(b_andre!=0) else False
+    b=True if(b_bruno!=0) else False
+    c=True if(b_clara!=0) else False
+    for i in range(3 if(b_andre!=0 and b_bruno!=0 and b_clara!=0) else 2):
         resultado_rodada=input()
         if(a):
             if(resultado_rodada=='acertou'):
@@ -18,7 +17,11 @@ while(op):
                 erro_a+=1
                 if(erro_a==3):
                     b_andre=0
-        if(b):
+                    if(b_bruno==0 or b_clara==0):
+                    	i=178
+                    	op=False
+            a=False
+        elif(b):
             if(resultado_rodada=='acertou'):
                 b_bruno+=2
                 b_andre-=1 if(b_andre>=1) else 0
@@ -27,7 +30,11 @@ while(op):
                 erro_b+=1
                 if(erro_b==3):
                     b_bruno=0
-        if(c):
+                    if(b_andre==0 or b_clara==0):
+                    	i=18
+                    	op=False
+            b=False
+        elif(c):
             if(resultado_rodada=='acertou'):
                 b_clara+=2
                 b_bruno-=1 if(b_bruno>=1) else 0
@@ -36,3 +43,8 @@ while(op):
                 erro_c+=1
                 if(erro_c==3):
                     b_clara=0
+                    if(b_andre==0 or b_bruno==0):
+                    	i=400
+                    	op=False
+            c=False
+	if(op==False): print('')
