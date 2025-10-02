d4dcaf987ef7155643d851583d6e14f763f4c8a6
ijsn <ijsn@g5c08.cin.ufpe.br>
2025-10-02 16:46:40 -0300

    clara

--- lista 2/L2Q05.py
+++ lista 2/L2Q05.py
@@ -8,6 +8,7 @@ erro_c=0
 a=True
 b=True
 c=True
+g1=0;g2=0;g3=0
 gambiarra=''
 while(op):
     for i in range(3 if(b_andre!=0 and b_bruno!=0 and b_clara!=0) else 2):
@@ -22,9 +23,8 @@ while(op):
             else:
                 erro_a+=1
                 if(erro_a==3):
-                    b_andre=0
                     print('andre perdeu feio')
-                    if(b_bruno==0 or b_clara==0):
+                    if((b_bruno==0 or b_clara==0) or erro_b==3 or erro_c==3):
                         i=10
                         op=False
             a=False
@@ -37,9 +37,8 @@ while(op):
             else:
                 erro_b+=1
                 if(erro_b==3):
-                    b_bruno=0
                     print('bruno perdeu feio')
-                    if(b_andre==0 or b_clara==0):
+                    if(b_andre==0 or b_clara==0 or erro_a==3 or erro_c==3):
                         i=10
                         op=False
             b=False
@@ -52,9 +51,8 @@ while(op):
             else:
                 erro_c+=1
                 if(erro_c==3):
-                    b_clara=0
                     print('clara perdeu feio')
-                    if(b_andre==0 or b_bruno==0):
+                    if((b_andre==0 or b_bruno==0) or erro_a==3 or erro_b==3):
                         i=10
                         op=False
             c=False
@@ -65,15 +63,18 @@ while(op):
             op=False
             i=718
     else:
-        if(b_andre==0 and erro_a<3 and a!=False):
+        if(b_andre==0 and erro_a<3 and g1==0):
             print('andre saiu do jogo')
-        elif(b_bruno==0 and erro_b<3 and b!=False):
+            g1=1
+        elif(b_bruno==0 and erro_b<3 and g2==0):
             print('bruno saiu do jogo')
-        elif(b_clara==0 and erro_c<3 and c!=False):
+            g2=1
+        elif(b_clara==0 and erro_c<3 and g3==0):
             print('clara saiu do jogo')
-    a=True if(b_andre!=0) else False
-    b=True if(b_bruno!=0) else False
-    c=True if(b_clara!=0) else False
+            g3=1
+    a=True if(b_andre!=0 and erro_a!=3) else False
+    b=True if(b_bruno!=0 and erro_b!=3) else False
+    c=True if(b_clara!=0 and erro_c!=3) else False
 else:
     if(c==False and b==False):
         print('andre ganhou')
