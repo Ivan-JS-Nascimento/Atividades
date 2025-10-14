e8e976d916daed202560906194604d7df319417f
Ivan-JS-Nascimento <ijsn@cin.ufpe.br>
2025-10-14 10:57:28 -0300

    lua

--- lista 3/L3Q02.py
+++ lista 3/L3Q02.py
@@ -4,15 +4,12 @@ comidas = []
 valor = []
 
 for i in range(num_convidados):
-    
     nome_convidado = input()
 
-    if nome_convidado != 'Maicon Kuster' :
-
+    if nome_convidado != 'Maicon Kuster':
         comida_convidado = input()
 
         if comida_convidado not in comidas:
-
             valor_comida = int(input())
 
             convidados.append(nome_convidado)
@@ -22,3 +19,13 @@ for i in range(num_convidados):
             print(f"Na Festa do Calabreso não pode comida Repetida SAI FORA {nome_convidado}")
     else:
         print("você é convidado DE GUÊÊ???, sai da minha festa seu FOFOQUEIRO!!")
+
+if len(convidados) == 0 :
+    print("Nenhum convidado marcou presença na festa do calabreso!")
+else:
+    print(f"Obrigado para o(a) {convidados[valor.index(max(valor))]} pelo(a) excelente {comidas[valor.index(max(valor))]}")
+    if len(convidados) > 1:
+        print(f"Rapaz, {convidados[valor.index(min(valor))]} trouxe o(a) {comidas[valor.index(min(valor))]} que estava altamente mais ou menos!!!")
+
+    for i in len(valor):
+        
