9db8bc3c2e610ab31c157b900e1cd364683e7d0e
Ivan-JS-Nascimento <ijsn@cin.ufpe.br>
2025-11-26 10:34:48 -0300

    questao 2

--- lista 6/L6Q02.py
+++ lista 6/L6Q02.py
@@ -4,16 +4,21 @@ exigencias = {}
 op = True
 
 while op:
+    # entrada crua
     auxi = input()
 
     if auxi != 'MIMOS RECEBIDOS':
         # fase 1
-        auxi = auxi.split(': ')
-        if auxi[0] == 'Bebidas' and auxi[1] == "latte":
-            auxi2 = int(auxi[2]) +1
-            auxi[2] = auxi2
 
-        exigencias[auxi[0]] = (auxi[1], auxi[2])
+        # auxi = [ 0 - categoria , 1 - item , 2 - quantidade ]
+        auxi = auxi.split(': ')
+
+        # se for uma bebida lattet tem que adicionas +1 na quantidade
+        if auxi[0] == 'Bebidas' and auxi[1] == "latte":
+            auxi[2] = int(auxi[2]) + 1
+        
+        # exigencias = { chave = categoria : valor = tupla( item , quantidade , quant_chegaram) }
+        exigencias[auxi[0]] = (auxi[1], auxi[2], 0)
 
     else:
         op = False
@@ -25,13 +30,59 @@ while op:
     categoria = ''
     item = ''
 
+    # entrada crua. ex:. Chegaram {quantidade} itens da categoria {categoria} (Item: {item_principal})
     chegaram = input()
 
     if chegaram != "ACABOU, a Glinda está pronta!":
-        chegaram = chegaram.split()
+
+        chegaram = chegaram.split(' ')
+
         quantidade = int(chegaram[1])
         categoria = chegaram[5]
         item = chegaram[7][:-1]
         
+        valor = exigencias.get(categoria, 'nao existe')
+
+        if valor != 'nao existe':
+            quant_anterior = valor[1]
+            quant_chegaram = valor[2]
+
+            exigencias.update( {categoria : ( item , quant_anterior-quantidade , quant_chegaram+quantidade) } )
+
     else:
-        ...
+        op = False
+
+
+print('Relatório de Balanço Final:')
+quant_catego_negativas = 0
+for key in exigencias:
+    frase = 'Você entregou TUDO! O mimo tá mais que garantido.' if( exigencias[key][1] <= 0 )else f'Golpe BAIXÍSSIMO! Faltam {exigencias[key][1]} mimos. Corre!'
+
+    if frase != 'Você entregou TUDO! O mimo tá mais que garantido.':
+        quant_catego_negativas += 1
+
+
+    print(f'Chegaram {exigencias[key][2]} itens da categoria {key} (Item: {exigencias[key][0]})')
+print()
+
+make = exigencias.get('Maquiagem','ne') 
+if make != 'ne' and make[0] == 'Gloss':
+    if make[1]<=0:
+        print('TUDO! O Gloss tá on. O look de Glinda tá salvo!')
+    else:
+        print('CADÊ meu gloss? Como divarei? ... A Glinda tá chorando de raiva!')
+
+bebida = exigencias.get('Bebidas','ne')
+if bebida != 'ne' and bebida[0] == 'latte':
+    if bebida[1]<=0:
+        print('Latte gelado pronto! A voz de Glinda está salva. Pode vir o próximo take')
+    else:
+        print('Cadeia neles! Faltou o Mimo Sagrado. Essa equipe tá perdida!')
+
+print()
+print('Veredito Final')
+
+if quant_catego_negativas >= 3:
+    print('Thank U, Next! A equipe de camarim foi demitida!')
+else:
+    print('Estoque Aprovado! Glinda vai brilhar em Wicked!')
